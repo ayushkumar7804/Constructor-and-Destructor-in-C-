bfa2965a2fc96e3e7e304bbe115a2d4483e3c14f
KassArc <ayushkumaryadav2029@gmail.com>
2025-09-02 17:01:25 +0530

    Create ConstructorOutsideTheClass.cpp

---
+++ ConstructorOutsideTheClass.cpp
@@ -0,0 +1,39 @@
+// Ayush Yadav
+// 24070123028
+
+#include <iostream>
+using namespace std;
+
+class Student {
+    string name;
+    double fee;
+
+public:
+    Student();
+    void display();
+};
+
+Student::Student() {
+    cout << "Enter the name of Student: ";
+    cin >> name;
+    cout << "Enter the fee: ";
+    cin >> fee;
+}
+
+void Student::display() {
+    cout << "\nStudent Name: " << name;
+    cout << "\nFee: " << fee << endl;
+}
+
+int main() {
+    Student s;
+    s.display();
+    return 0;
+}
+
+// OUTPUT
+// Enter the name of Student: Ayush
+// Enter the fee: 156000
+
+// Student Name: Ayush
+// Fee: 156000
